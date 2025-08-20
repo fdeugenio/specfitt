a45cf7d5c81d376a67fee3c741108f7db4066795
Francesco D'Eugenio <fdeugenio@gmail.com>
2025-08-20 15:00:02 +0100

    forgot inital commit

--- specfitt/__init__.py
+++ specfitt/__init__.py
@@ -3,3 +3,4 @@ from .specfitt import *
 from . import specfitt_irony as irony
 from . import specfitt_irony_out as ironyout
 from . import specfitt_irony_out_untie2Gauss as ironyout_untie2Gauss
+from . import specfitt_darkhorse
